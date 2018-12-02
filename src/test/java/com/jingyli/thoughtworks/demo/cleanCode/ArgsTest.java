0772b7c69177a4e1febfd95b650ee08c6e7488ed
jingyli <jingyli@thoughtworks.com>
2018-12-02 19:51:08 +0800

    [Jingyue] add test of Args function

---
+++ src/test/java/com/jingyli/thoughtworks/demo/cleanCode/ArgsTest.java
@@ -0,0 +1,42 @@
+package com.jingyli.thoughtworks.demo.cleanCode;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ArgsTest {
+    @Test
+    public void withLoggingEnabled() throws Exception {
+        Args arg = new Args("l", new String[]{"-l"});
+        boolean logging = arg.getBoolean('l');
+        assertThat("Logging", logging, is(true));
+    }
+
+    @Test
+    public void withSetPort() throws Exception {
+        Args arg = new Args("p#", new String[]{"-p", "8080"});
+        int port = arg.getInt('p');
+        assertThat("Port", port, is(8080));
+    }
+
+    @Test
+    public void withConfiguredDirectory() throws Exception {
+        Args arg = new Args("d*", new String[]{"-d", "C:/Temp"});
+        String directory = arg.getString('d');
+        assertThat("Directory", directory, is(equalTo("C:/Temp")));
+    }
+
+    @Test
+    public void withLoggingEnabledSetPortAndConfiguredDirectory() throws Exception {
+        Args arg = new Args("l,p#,d*", new String[]{"-d", "C:/Temp", "-p", "8080", "-l"});
+
+        boolean logging = arg.getBoolean('l');
+        int port = arg.getInt('p');
+        String directory = arg.getString('d');
+
+        assertThat("Logging", logging, is(true));
+        assertThat("Port", port, is(8080));
+        assertThat("Directory", directory, is(equalTo("C:/Temp")));
+    }
+}
