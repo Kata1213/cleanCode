a9fcbf80d4f9d44d5b802d3e68c8cc0ca1044ce9
jingyli <jingyli@thoughtworks.com>
2018-12-02 18:33:53 +0800

    [Jingyue] add simple use of Args in main

--- src/main/java/com/jingyli/thoughtworks/demo/cleanCode/CleanCodeApplication.java
+++ src/main/java/com/jingyli/thoughtworks/demo/cleanCode/CleanCodeApplication.java
@@ -7,7 +7,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CleanCodeApplication {
 
 	public static void main(String[] args) {
+		try {
+			Args arg= new Args("l,p#,d*",args);
+			boolean logging=arg.getBoolean('l');
+			int port =arg.getInt('p');
+			String directory =arg.getString('d');
+			executeApplication(logging,port,directory);
+		}catch (Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
 
-
+	private static void executeApplication(boolean logging, int port, String directory) {
+		System.out.println("logging:" + logging + ", port: " + port + ", directory " + directory);
 	}
 }
