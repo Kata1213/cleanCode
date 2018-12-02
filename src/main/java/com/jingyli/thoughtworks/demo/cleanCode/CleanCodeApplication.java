229fee950038f72d0036eb19a93e18e5a8d3950a
jingyli <jingyli@thoughtworks.com>
2018-12-02 18:17:46 +0800

    [Jingyue] init cleancode project

---
+++ src/main/java/com/jingyli/thoughtworks/demo/cleanCode/CleanCodeApplication.java
@@ -0,0 +1,13 @@
+package com.jingyli.thoughtworks.demo.cleanCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCodeApplication {
+
+	public static void main(String[] args) {
+
+
+	}
+}
---
+++ src/test/java/com/jingyli/thoughtworks/demo/cleanCode/CleanCodeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jingyli.thoughtworks.demo.cleanCode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CleanCodeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
